e7c9c2f974faba25e1f19e9c702770e590f9a84a
tieshort <razumoffgleb@gmail.com>
2024-07-01 15:13:15 +0300

    structured

--- src/main.py
+++ src/main.py
@@ -1,15 +1,25 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 
-from db.database import create_all_models
-
-from routes import servers
+from order.router import router as order_router
+from order.models import Base
+from database import engine
 
 import uvicorn
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
 
-app.include_router(servers.router)
+    await engine.dispose()
+    print("Application shutdown complete")
+
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(order_router)
 
 if __name__ == "__main__":
     uvicorn.run('main:app', reload=True)
---
+++ src/order/router.py
@@ -0,0 +1,58 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy import select, insert
+from sqlalchemy.orm import Session
+
+from .schemas import UserOrderCreate, ImageRead, ImageCreate
+from .models import UserOrder, Image
+
+from database import get_async_session
+
+router = APIRouter(prefix="/servers")
+    
+@router.post("")
+async def create_server(post: UserOrderCreate, session: Session = Depends(get_async_session)):
+    quiery = insert(UserOrder).values(**post.model_dump())
+    await session.execute(quiery)
+    await session.commit()
+    return {"status": "succes"}
+
+@router.get("/images", response_model=list[ImageRead])
+async def get_image_templates(session: Session = Depends(get_async_session)):
+    quiery = select(Image)
+    try:
+        result = await session.execute(quiery)
+        return {
+                "status": "success",
+                "data": result.all(),
+                "details": None
+            }
+    except Exception:
+        raise HTTPException(status_code=500, detail={
+            "status": "error",
+            "data": None,
+            "details": None
+        })
+
+@router.get("/my-images", response_model=list[ImageRead])
+async def get_my_images(session: Session = Depends(get_async_session)):
+    quiery = select(Image)
+    try:
+        result = await session.execute(quiery)
+        return {
+                "status": "success",
+                "data": result.all(),
+                "details": None
+            }
+    except Exception:
+        raise HTTPException(status_code=500, detail={
+            "status": "error",
+            "data": None,
+            "details": None
+        })
+
+@router.post("/my-images/create")
+async def create_image(image: ImageCreate, session: Session = Depends(get_async_session)):
+    quiery = insert(Image).values(**image.model_dump())
+    await session.execute(quiery)
+    await session.commit()
+    return {"status": "succes"}
