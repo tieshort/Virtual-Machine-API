4b0e8f5cd33a95583c44c2bacb5330da5220f58e
tieshort <razumoffgleb@gmail.com>
2024-07-04 10:09:43 +0300

    updated router, minor changes for database, main

--- src/main.py
+++ src/main.py
@@ -3,18 +3,18 @@ from contextlib import asynccontextmanager
 import uvicorn
 from fastapi import FastAPI
 
-from database import get_engine
+from database import engine
 from order.models import Base
 from order.router import router as order_router
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with get_engine().begin() as conn:
+    async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
     yield
 
-    await get_engine().dispose()
+    await engine.dispose()
     print("Application shutdown complete")
 
 
