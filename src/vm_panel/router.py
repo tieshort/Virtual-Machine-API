404a872a63b783ad33bdddf015cdf854af8a1f63
tieshort <razumoffgleb@gmail.com>
2024-07-05 10:25:33 +0300

    fixed router template

--- src/vm_panel/router.py
+++ src/vm_panel/router.py
@@ -1,23 +1,29 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi.middleware import Middleware
+from starlette.middleware.base import BaseHTTPMiddleware
 
-router = APIRouter(prefix="{user_id}/vms")
+router = APIRouter(prefix="/{user_id}/vms")
 
 @router.get("")
-async def get_vms():
+async def get_vms(user_id: int):
     pass
 
 @router.get("/{vm_id}/panel/config")
-async def get_panel_config(vm_id: str):
+async def get_panel_config(user_id: int, vm_id: str):
+    pass
+
+@router.get("/{vm_id}/panel/console")
+async def get_panel_console(user_id: int, vm_id: str):
     pass
 
 @router.post("/{vm_id}/panel/start_vm")
-async def start_vm(vm_id: str):
+async def start_vm(user_id: int, vm_id: str):
     pass
 
 @router.post("/{vm_id}/panel/stop_vm")
-async def stop_vm(vm_id: str):
+async def stop_vm(user_id: int, vm_id: str):
     pass
 
 @router.post("/{vm_id}/panel/retart_vm")
-async def retart_vm(vm_id: str):
+async def retart_vm(user_id: int, vm_id: str):
     pass
