18722a690353d7e0c1c022a5ec6e6f24a22a47f6
tieshort <razumoffgleb@gmail.com>
2024-07-07 23:23:50 +0300

    removed unused import

--- src/database.py
+++ src/database.py
@@ -1,6 +1,6 @@
 from typing import AsyncGenerator
 
-from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, create_async_engine
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
 from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
